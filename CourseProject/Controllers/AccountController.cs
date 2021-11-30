80c4d6ced00c182d337695b2ab4d7e27d65fa5de
Yanpa <panayot1806@gmail.com>
2021-11-30 15:00:39 +0200

    Adding login and register views

---
+++ CourseProject/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using CourseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ CourseProject/Utility/InformationClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Utility
+{
+    public class InformationClass
+    {
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value = InformationClass.Admin, Text=InformationClass.Admin},
+                new SelectListItem{Value = InformationClass.Patient, Text=InformationClass.Patient},
+                new SelectListItem{Value = InformationClass.Doctor, Text=InformationClass.Doctor}
+            };
+        }
+    }
+}
